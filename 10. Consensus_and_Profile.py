05cd274d50851862879e6ba5bdb1d0acf7f4a339
Lucas Cortes <55763536+lucascortes96@users.noreply.github.com>
2020-09-22 11:38:58 -0400

    Create 10. Consensus_and_Profile.py

---
+++ 10. Consensus_and_Profile.py
@@ -0,0 +1,46 @@
+## Kind of a brute force solution to the problem, had to create a consensus string and a matrix for the given sequences 
+
+from Bio import SeqIO  
+import numpy
+                 
+# Opening file and create a sequence list from FASTA    
+sequences = []                             
+handle = open('rosalind_cons (1).txt', 'r')     
+for record in SeqIO.parse(handle, 'fasta'):
+     sequence = []                         
+     for nt in record.seq:                 
+          sequence.extend(nt)              
+     sequences.append(sequence)            
+handle.close()    
+
+import numpy   
+# Count number of nucleotides occuring at each position                                               
+profile = numpy.zeros((4, len(sequences[0])), dtype=numpy.int)
+for i,line in enumerate(sequences):                           
+     for j, nt in enumerate(line):                            
+          if nt == 'A':                                       
+               profile[0][j] += 1                             
+          elif nt == 'C':                                     
+               profile[1][j] += 1                             
+          elif nt == 'G':                                     
+               profile[2][j] += 1                             
+          elif nt == 'T':                                     
+               profile[3][j] += 1  
+
+# Create consensus sequence
+consensus = ''                                                  
+for A,C,G,T in zip(profile[0],profile[1],profile[2],profile[3]):
+     if A >= C and A >= G and A >= T:                           
+          consensus += 'A'                                      
+     elif C >= A and C >= G and C >= T:                         
+          consensus += 'C'                                      
+     elif G >= A and G >= C and G >= T:                         
+          consensus += 'G'                                      
+     elif T >= A and T >= C and T >= G:                         
+          consensus += 'T'          
+# Print
+print(consensus)                                   
+print('A: ' + ' '.join(str(e) for e in profile[0]))
+print('C: ' + ' '.join(str(e) for e in profile[1]))
+print('G: ' + ' '.join(str(e) for e in profile[2]))
+print('T: ' + ' '.join(str(e) for e in profile[3]))
